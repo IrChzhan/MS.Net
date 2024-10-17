905783d65b2b103279b2c3c1b64e311ad6dc8f5d
Irina Chzhan <chzhanira@yandex.ru>
2024-10-17 09:19:11 +0300

    add skeleton

--- Program.cs
+++ Program.cs
@@ -1,25 +1,14 @@
+using MS.Net.Service.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+SerilogConfig.ConfigureService(builder);
+SwaggerConfig.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+SerilogConfig.ConfigureApplication(app);
+SwaggerConfig.ConfigureApplication(app);
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
 app.Run();
---
+++ Service/IoC/SwaggerConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace MS.Net.Service.IoC;
+
+public class SwaggerConfig
+{
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+    
+    public static void ConfigureApplication(IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+}
